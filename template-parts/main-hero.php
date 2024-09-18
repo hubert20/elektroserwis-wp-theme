0d66a8a7335b10de4ad70f7dcc17b833af89480f
Hubert <hubert@tomedia.pl>
2024-09-19 00:18:42 +0200

    Structure mainpage

--- template-parts/main-hero.php
+++ template-parts/main-hero.php
@@ -5,12 +5,50 @@ $hero_background_mobile = get_field('hero_background_mobile');
 
 
 <div id="hero" class="top-hero position-relative d-flex justify-content-center flex-column">
-    <div class="hro-background-overlay">
+    <div class="top-hero__overlay d-flex align-items-center">
 
         <!-- Hero Cnt -->
-        <div class="container top-hero-cnt position-relative">
+        <div class="container top-hero__cnt position-relative">
+            <!-- Hero slider -->
+            <div class="top-hero__slider">
+                <!-- Slider main container -->
+                <div class="swiper swiper-container main-swiper">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <?php if (have_rows('hero_cnt_slider')) : ?>
+                            <?php
+                            while (have_rows('hero_cnt_slider')) : the_row();
+                                $hero_img = get_sub_field('hero_cnt_image');
+                                $hero_cnt = get_sub_field('hero_cnt_slider');
+                            ?>
+                                <div class="swiper-slide">
+                                    <div class="row justify-content-center align-items-center">
+                                        <div class="col-lg-6">
+                                            <?php echo $hero_cnt; ?>
+                                        </div>
+                                        <div class="col-lg-4 text-center">
+                                            <?php if ($hero_img) : ?>
+                                                <img src="<?php echo $hero_img['url']; ?>" alt="<?php echo $hero_img['alt']; ?>" class="img-fluid mx-auto">
+                                            <?php endif; ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+                    <!-- If we need pagination -->
+                    <div class="swiper-pagination"></div>
 
+                    <!-- If we need navigation buttons -->
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
 
+                    <!-- If we need scrollbar -->
+                    <div class="swiper-scrollbar"></div>
+                </div>
+
+            </div>
         </div>
         <!-- Scroll Anchor-->
         <a href="#iframe-hauses" class="tst-scroll-hint-frame tst-anchor-scroll">
