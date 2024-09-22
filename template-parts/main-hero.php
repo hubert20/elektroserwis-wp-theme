758af7e062c8bc2cc84abc459a35dda715f08bf1
Hubert <hubert@tomedia.pl>
2024-09-22 23:33:27 +0200

    Update main-hero.php

--- template-parts/main-hero.php
+++ template-parts/main-hero.php
@@ -22,10 +22,10 @@ $hero_background_mobile = get_field('hero_background_mobile');
                         ?>
                             <div class="swiper-slide">
                                 <div class="row justify-content-center align-items-center">
-                                    <div class="col-lg-6">
+                                    <div class="col-lg-6 mb-3 mb-lg-0">
                                         <?php echo $hero_cnt; ?>
                                     </div>
-                                    <div class="col-lg-4 text-center">
+                                    <div class="col-8 col-lg-4 text-center">
                                         <?php if ($hero_img) : ?>
                                             <img src="<?php echo $hero_img['url']; ?>" alt="<?php echo $hero_img['alt']; ?>" class="img-fluid mx-auto">
                                         <?php endif; ?>
