0a990827a1d6a24cdb6c57c48253389b823f420a
Hubert <hubert@tomedia.pl>
2024-09-26 22:17:05 +0200

    Mainpage

--- template-parts/main-form.php
+++ template-parts/main-form.php
@@ -1,15 +1,13 @@
 <!-- Contact `Form -->
-<section class="form-box py-4 py-lg-5 bg-orange" style="height: 600px;">
+<section class="form-box py-4 py-lg-5">
     <div class="container">
         <div class="position-relative">
-            <h2 class="title-stroke title-stroke--dark fw-bold chakra-petch-font standard-title-1 text-uppercase">Formularz kontaktowy</h2>
-            <h2 class="subtitle chakra-petch-font text-center standard-title-2">Wyślij zapytanie</h2>
+            <h2 class="title-stroke title-stroke--white fw-bold chakra-petch-font standard-title-3 text-uppercase text-center mb-0">Formularz kontaktowy</h2>
+            <h2 class="subtitle chakra-petch-font text-center standard-title-4 fw-bolder mb-3 mb-lg-5 text-white">Wyślij zapytanie</h2>
         </div>
         <div class="form-box--cnt">
             <!-- Contact form -->
-
-    
-
+            <?php echo apply_shortcodes('[contact-form-7 id="7c3e932" title="Zapytanie"]'); ?>
         </div>
     </div>
 </section>
--- template-parts/main-jobs.php
+++ template-parts/main-jobs.php
@@ -1,34 +1,34 @@
 <!-- Jobs counter section -->
-<section class="counter-main-box py-4 py-lg-5" id="counter">
+<section class="counter-main-box py-4 py-lg-5 my-lg-5" id="counter">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-3">
                 <div class="position-relative counter-number--box">
-                    <img src="http://localhost:81/wordpress/wp-content/uploads/2024/09/naprawa-alternatorow.png" class="img-fluid counter-number--icon" alt="">
+                    <img src="/wp-content/uploads/2024/09/naprawa-alternatorow.png" class="img-fluid counter-number--icon" alt="">
                     <p class="count counter-number counter-number--plus standard-title-3 chakra-petch-font fw-bold" data-count="6000">0</p>
                 </div>
-                <p class="text-uppercase fw-bold">Naprawionych alternatorów</p>
+                <p class="text-uppercase fw-bold counter-main-box--titles lh-13">Naprawionych alternatorów</p>
             </div>
             <div class="col-lg-3">
                 <div class="position-relative counter-number--box">
-                    <img src="http://localhost:81/wordpress/wp-content/uploads/2024/09/naprawa-rozrusznikow.png" class="img-fluid counter-number--icon" alt="">
+                    <img src="/wp-content/uploads/2024/09/naprawa-rozrusznikow.png" class="img-fluid counter-number--icon" alt="">
                     <p class="count counter-number counter-number--plus standard-title-3 chakra-petch-font fw-bold" data-count="7000">0</p>
                 </div>
-                <p class="text-uppercase fw-bold">Naprawionych rozruszników</p>
+                <p class="text-uppercase fw-bold counter-main-box--titles lh-13">Naprawionych rozruszników</p>
             </div>
             <div class="col-lg-3">
                 <div class="position-relative counter-number--box">
-                    <img src="http://localhost:81/wordpress/wp-content/uploads/2024/09/klimatyzacja-naprawa-i-serwis.png" class="img-fluid counter-number--icon" alt="">
+                    <img src="/wp-content/uploads/2024/09/klimatyzacja-naprawa-i-serwis.png" class="img-fluid counter-number--icon" alt="">
                     <p class="count counter-number counter-number--plus standard-title-3 chakra-petch-font fw-bold" data-count="8000">0</p>
                 </div>
-                <p class="text-uppercase fw-bold">Naprawionych układów klimatyzacji</p>
+                <p class="text-uppercase fw-bold counter-main-box--titles lh-13">Naprawionych układów klimatyzacji</p>
             </div>
             <div class="col-lg-3">
                 <div class="position-relative counter-number--box">
-                    <img src="http://localhost:81/wordpress/wp-content/uploads/2024/09/zadowoleni-klienci.png" class="img-fluid counter-number--icon" alt="">
+                    <img src="/wp-content/uploads/2024/09/zadowoleni-klienci.png" class="img-fluid counter-number--icon" alt="">
                     <p class="count counter-number counter-number--plus standard-title-3 chakra-petch-font fw-bold" data-count="9000">0</p>
                 </div>
-                <p class="text-uppercase fw-bold">Zadowolonych klientów</p>
+                <p class="text-uppercase fw-bold counter-main-box--titles lh-13">Zadowolonych klientów</p>
             </div>
         </div>
     </div>
