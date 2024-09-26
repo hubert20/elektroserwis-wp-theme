98a43152e1dafbda2a6dbea4e555d0d1f6eb8a72
Hubert <hubert@tomedia.pl>
2024-09-26 18:09:13 +0200

    Main Page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require("bootstrap");
 import Swiper from "swiper"
-import {Navigation} from 'swiper/modules'
+import { Navigation } from 'swiper/modules'
 
 
 (function () {
@@ -25,4 +25,42 @@ import {Navigation} from 'swiper/modules'
     }
   });
 
+// Scroll Counter number
+  var counted = 0;
+  window.addEventListener('scroll', function () {
+
+      var counterElement = document.getElementById('counter');
+      var oTop = counterElement.offsetTop - window.innerHeight;
+
+      if (counted === 0 && window.scrollY > oTop) {
+          var countElements = document.querySelectorAll('.count');
+          countElements.forEach(function (countElement) {
+              var countTo = parseInt(countElement.getAttribute('data-count'));
+              var currentCount = parseInt(countElement.textContent);
+
+              var startCount = { countNum: currentCount };
+
+              var duration = 2000;
+              var startTime = null;
+
+              function animateCount(timestamp) {
+                  if (!startTime) startTime = timestamp;
+                  var progress = timestamp - startTime;
+                  var stepCount = Math.min(progress / duration, 1) * (countTo - currentCount) + currentCount;
+
+                  countElement.textContent = Math.floor(stepCount);
+
+                  if (progress < duration) {
+                      requestAnimationFrame(animateCount);
+                  } else {
+                      countElement.textContent = countTo; // Ustaw ostateczną wartość
+                  }
+              }
+
+              requestAnimationFrame(animateCount);
+          });
+
+          counted = 1;
+      }
+  });
 })();
--- template-parts/main-form.php
+++ template-parts/main-form.php
@@ -1,8 +1,15 @@
-<!-- Form -->
-<section class="form-box py-4 py-lg-5 bg-orange">
+<!-- Contact `Form -->
+<section class="form-box py-4 py-lg-5 bg-orange" style="height: 600px;">
     <div class="container">
+        <div class="position-relative">
+            <h2 class="title-stroke title-stroke--dark fw-bold chakra-petch-font standard-title-1 text-uppercase">Formularz kontaktowy</h2>
+            <h2 class="subtitle chakra-petch-font text-center standard-title-2">Wyślij zapytanie</h2>
+        </div>
+        <div class="form-box--cnt">
+            <!-- Contact form -->
 
+    
 
-
+        </div>
     </div>
 </section>
